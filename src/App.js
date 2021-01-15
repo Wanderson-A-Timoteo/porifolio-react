0f67d5cc25db4717331fe1605cc5e14774f94b7f
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-01-15 10:13:11 -0400

    add component About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import About from "./components/About";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
 import Services from "./components/Services";
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Nav />
       <Services />
+      <About />
     </div>
   );
 }
